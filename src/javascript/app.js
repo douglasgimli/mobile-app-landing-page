b41c642c523052fcf241f63f24b427000e43ea81
DouglasGimli <douglasgimli@gmail.com>
2017-11-04 17:38:12 -0200

    Add: Initial project structure

---
+++ src/javascript/app.js
@@ -0,0 +1,9 @@
+// List your HTML dev files to Hot Reload it when changed
+if (process.env.NODE_ENV !== 'production') {
+  require('../../public/index.html')
+}
+
+// Import styles to be processed
+import styles from '../styles/styles.css'
+
+// Include and initialize JS
