c8c0d10f2d13479948b16a74d6bf5e4dff16ddff
DouglasGimli <douglasgimli@gmail.com>
2017-11-05 11:56:35 -0200

    Fix: JS and package json

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -1,8 +1,3 @@
-// List your HTML dev files to Hot Reload it when changed
-if (process.env.NODE_ENV !== 'production') {
-  require('../../public/index.html')
-}
-
 // Import styles to be processed
 import styles from '../styles/styles.css'
 
